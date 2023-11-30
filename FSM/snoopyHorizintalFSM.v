b9a33aacc16d539230f1078e24851e38344ff934
Riya Bansal <132302754+errban@users.noreply.github.com>
2023-11-30 10:30:24 -0500

    Update snoopyHorizintalFSM.v

--- FSM/snoopyHorizintalFSM.v
+++ FSM/snoopyHorizintalFSM.v
@@ -5,9 +5,9 @@ module snoopyHorizontalFSM (clock, reset, input_left, input_right, snoopy_x);
     output [7:0] snoopy_x;
 
     reg [7:0] x_pos;
-    reg x_speed;
+    //reg [1:0] x_speed;
 
-    reg [1:0] state;
+    reg state;
 
     localparam S_LEFT = 2'b01,
                 S_RIGHT = 2'b10,
@@ -18,7 +18,7 @@ module snoopyHorizontalFSM (clock, reset, input_left, input_right, snoopy_x);
     always @ (posedge clock)
     begin : next_state_and_output_logic
         if (reset) begin
-            x_speed <= 0;
+            //x_speed <= 0;
             x_pos <= 0;
             state <= S_IDLE_X;
         end
@@ -27,23 +27,23 @@ module snoopyHorizontalFSM (clock, reset, input_left, input_right, snoopy_x);
                 S_IDLE_X: begin
                     if (input_left) begin
                         state <= S_LEFT;
-                        x_speed <= -1; //adjust
+                        //x_speed <= 1; //adjust
                     end 
                     else if (input_right) begin
                         state <= S_RIGHT;
-                        x_speed <= 1; //adjust
+                        //x_speed <= 1; //adjust
                     end 
                 end
                 S_LEFT : begin
                     if (!input_left) begin
                         state <= S_IDLE_X;
-                        x_speed <= 0;
+                        //x_speed <= 0;
                     end
                 end
                 S_RIGHT: begin
                     if (!input_right) begin
                         state <= S_IDLE_X;
-                        x_speed <= 0;
+                        //x_speed <= 0;
                     end
                 end
             endcase
@@ -51,19 +51,25 @@ module snoopyHorizontalFSM (clock, reset, input_left, input_right, snoopy_x);
     end
 
     // Update Snoopy's position with bounds
-    always @ (posedge clock) begin
-        if (reset) begin
-            x_pos <= 0; // Reset x_pos
-        end else begin
-            if (x_pos + x_speed > MAX_X_POS) begin
-                x_pos <= MAX_X_POS; // Prevent going beyond the maximum
-            end else if (x_pos + x_speed < 0) begin
-                x_pos <= 0; // Prevent going below 0
-            end else begin
-                x_pos <= x_pos + x_speed; // Normal position update
+always @(posedge clock) begin
+    if (reset) begin
+        x_pos <= 0; // Reset x_pos
+    end else begin
+        case (state)
+            S_LEFT: begin
+                if (x_pos > 0) begin
+                    x_pos <= x_pos - 1; // Move left, decrement x_pos
+                end
             end
-        end
+            S_RIGHT: begin
+                if (x_pos < MAX_X_POS) begin
+                    x_pos <= x_pos + 1; // Move right, increment x_pos
+                end
+            end
+            // Handle other states as needed
+        endcase
     end
+end
 
     //assign output coordinate
     assign snoopy_x = x_pos;
