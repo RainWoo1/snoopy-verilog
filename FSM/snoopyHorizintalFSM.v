3f29e3b94b71e275eb755fbee25b718fa9360b26
Riya Bansal <132302754+errban@users.noreply.github.com>
2023-12-02 18:23:53 -0500

    Update snoopyHorizintalFSM.v

--- FSM/snoopyHorizintalFSM.v
+++ FSM/snoopyHorizintalFSM.v
@@ -19,7 +19,7 @@ module snoopyHorizontalFSM (clock, reset, input_left, input_right, snoopy_x);
     begin : next_state_and_output_logic
         if (reset) begin
             //x_speed <= 0;
-            x_pos <= 0;
+           // x_pos <= 0;
             state <= S_IDLE_X;
         end
         else begin
