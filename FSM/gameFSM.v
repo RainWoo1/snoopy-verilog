1544ef46ab67213c8e28ebd2294a8543f546951f
Riya Bansal <132302754+errban@users.noreply.github.com>
2023-12-03 08:20:25 -0500

    Update gameFSM.v

--- FSM/gameFSM.v
+++ FSM/gameFSM.v
@@ -1,67 +1,61 @@
-module gameFSM (reset, clock, collided, reached_screen_end, user_input);
+module gameFSM (reset, clock, collided, reached_screen_end, user_input, col);
 
     localparam S_BEGIN = 2'b00,
-                S_CONTINUE = 2'b01,
-                S_LOST = 2'b10,
-                S_WON = 2'b11 ;
+               S_CONTINUE = 2'b01,
+               S_LOST = 2'b10,
+               S_WON = 2'b11;
 
-    input collided, reached_screen_end, user_input; //user input is a boolean value (=1 when user is providing some input )
-    input reset, clock;
+    input collided, reached_screen_end, user_input, reset, clock;
+    output reg [2:0] col;
     reg [1:0] current_state, next_state;
 
-    always @ (*)  //next state logic
-    begin : state_table
-        if (reset) next_state = S_BEGIN;
-        case (current_state)
-            S_BEGIN : next_state = user_input ? S_CONTINUE : S_BEGIN;
+    // Next state logic
+    always @ (*) begin
+        if (!reset) begin
+            next_state = S_BEGIN;
+            col = 3'b110; // Default color
+        end else begin
+            case (current_state)
+                S_BEGIN:
+                    next_state = user_input ? S_CONTINUE : S_BEGIN;
 
-            S_CONTINUE : begin
-                if (collided)
-                    next_state = S_LOST;
-                else if (reached_screen_end)
-                    next_state = S_WON;
-            end
+                S_CONTINUE:
+                    if (collided)
+                        next_state = S_LOST;
+                    else if (reached_screen_end)
+                        next_state = S_WON;
+                    else
+                        next_state = S_CONTINUE;
 
-            S_LOST : next_state = S_BEGIN;
-            S_WON : next_state = S_BEGIN;
+                S_LOST:
+                    next_state = S_BEGIN;
 
-            default : next_state = S_BEGIN;
-        endcase
-    end 
+                S_WON:
+                    next_state = S_BEGIN;
 
-//what output to give from this module?
-	// State update logic
+                default:
+                    next_state = S_BEGIN;
+            endcase
+        end
+    end
+
+    // State update logic
     always @ (posedge clock) begin
-        if (reset) 
+        if (!reset)
             current_state <= S_BEGIN;
-        else 
+        else
             current_state <= next_state;
     end
-endmodule
 
-//module which returns collided = 1; reached_screen_end = 1
-module collision_end (x_coord, y_coord, colour, clock, resetn, collided, reached_screen_end);
-
-    input [6:0] y_coord; // Added x_coord as input
-	input [7:0] x_coord;
-    input clock, resetn;
-    input [2:0] colour;
-    output reg collided, reached_screen_end;
-
-    always @(posedge clock) begin
-        if (!resetn) begin
-            collided <= 1'b0;
-        end else if (colour == 3'b010) begin
-            collided <= 1'b1;
-        end
-        // No else clause
-    end
-
-    always @(posedge clock) begin
-        if (!resetn) begin
-            reached_screen_end <= 1'b0;
-        end else if (x_coord == 7'd159) begin // Check if x_coord is 159
-            reached_screen_end <= 1'b1;
-        end 
+    // Output logic based on state
+    always @ (*) begin
+        case (current_state)
+            S_LOST:
+                col = 3'b100; // Color for lost
+            S_WON:
+                col = 3'b010; // Color for won
+            default:
+                col = 3'b110; // Default color
+        endcase
     end
 endmodule
