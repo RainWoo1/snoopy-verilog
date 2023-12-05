daa3928c1e5ded03df7d00f0e5a167c1b039bf2b
Riya Bansal <132302754+errban@users.noreply.github.com>
2023-12-05 14:27:51 -0500

    Update snoopyHorizontalFSM.v

--- FSM/snoopyHorizontalFSM.v
+++ FSM/snoopyHorizontalFSM.v
@@ -1,10 +1,11 @@
-module snoopyHorizontalFSM (clock, reset, input_left, input_right, snoopy_x, score);
+module snoopyHorizontalFSM (clock, reset, input_left, input_right, snoopy_x, score, HEX0);
 
     input clock, reset;
     input input_left, input_right;
     output [7:0] snoopy_x;
-    output [3:0] score;
-
+	output reg [3:0] score;
+	output [6:0] HEX0;
+	
     reg [7:0] x_pos;
     //reg [1:0] x_speed;
 
@@ -14,15 +15,14 @@ module snoopyHorizontalFSM (clock, reset, input_left, input_right, snoopy_x, sco
                 S_RIGHT = 2'b10,
                 S_IDLE_X = 2'b00;
 
-    localparam MAX_X_POS = 160;
+    localparam MAX_X_POS = 155;
 
     always @ (posedge clock)
     begin : next_state_and_output_logic
-        if (reset) begin
+        if (!reset) begin
             //x_speed <= 0;
            // x_pos <= 0;
             state <= S_IDLE_X;
-            score <= 4'b0;
         end
         else begin
             case (state)
@@ -54,8 +54,8 @@ module snoopyHorizontalFSM (clock, reset, input_left, input_right, snoopy_x, sco
 
     // Update Snoopy's position with bounds
 always @(posedge clock) begin
-    if (reset) begin
-        x_pos <= 0; // Reset x_pos
+    if (!reset) begin
+        x_pos <= 8'd15; // Reset x_pos
     end else begin
         case (state)
             S_LEFT: begin
@@ -68,21 +68,26 @@ always @(posedge clock) begin
                     x_pos <= x_pos + 1; // Move right, increment x_pos
                 end
             end
+				//default: x_pos <= 8'd15;
+            // Handle other states as needed
         endcase
-        case (x_pos + 8'b1)  //+1 score after crossing one obstacle
-            8'd55 : score <= 4'd1;
-            8'd61 : score <= 4'd2;
-            8'd81 : score <= 4'd3;
-            8'd114 : score <= 4'd4;
-            8'd127 : score <= 4'd5;
-            8'd154 : score <= 4'd6;
-            default : score <= 4'd0;
-        endacase
-            
+//		  
+//		  		case (x_pos + 8'b1)  //+1 score after crossing one obstacle
+//					8'd40 : score <= 4'd1;
+//					8'd61 : score <= 4'd2;
+//					8'd81 : score <= 4'd3;
+//					8'd111 : score <= 4'd4;
+//					8'd127 : score <= 4'd5;
+//					8'd154 : score <= 4'd6;
+//					default : score <= 4'd0;
+//				endcase
     end
 end
 
     //assign output coordinate
     assign snoopy_x = x_pos;
+
+
+	//Hexadecimal_To_Seven_Segment Segment0 ( .hex_number(score), .seven_seg_display(HEX0));
 
 endmodule
