2246583d35d69b8b1c659102866bea75750c630d
Riya Bansal <132302754+errban@users.noreply.github.com>
2023-12-06 20:39:24 -0500

    Update and rename snoopy.v to toplevel.v

--- toplevel.v
+++ toplevel.v
@@ -1,4 +1,4 @@
-module snoopy
+module toplevel
 #(parameter CLOCK_FREQUENCY = 50000000)(
 		CLOCK_50,						//	On Board 50 MHz
 		KEY,							// On Board Keys
@@ -265,80 +265,4 @@ end
 		
 		PS2_Demo keyboard(CLOCK_50, KEY, PS2_CLK, PS2_DAT,LEDR, input_right, input_left, input_up);
 // 	snoopyCharacter2 v1 (.address(address), .clock(CLOCK_50), .q(colour) );
-endmodule
-
-
-module hex_decoder (c, display);
-
-	input[3: 0] c;
-	output[6: 0] display;
-
-	wire m0, ml, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15;
-
-	assign m0 = ~c[3] & ~c[2] & ~c[1] & ~c[0];
-	assign m1 = ~c[3] & ~c[2] & ~c[1] & c[0];
-	assign m2 = ~c[3] & ~c[2] & c[1] & ~c[0];
-	assign m3 = ~c[3] & ~c[2] & c[1] & c[0];
-	assign m4 = ~c[3] & c[2] & ~c[1] & ~c[0];
-	assign m5 = ~c[3] & c[2] & ~c[1] & c[0];
-	assign m6 = ~c[3] & c[2] & c[1] & ~c[0];
-	assign m7 = ~c[3] & c[2] & c[1] & c[0];
-	assign m8 = c[3] & ~c[2] & ~c[1] & ~c[0];
-	assign m9 = c[3] & ~c[2] & ~c[1] & c[0];
-	assign m10 = c[3] & ~c[2] & c[1] & ~c[0];
-	assign m11 = c[3] & ~c[2] & c[1] & c[0];
-	assign m12 = c[3] & c[2] & ~c[1] & ~c[0];
-	assign m13 = c[3] & c[2] & ~c[1] & c[0];
-	assign m14 = c[3] & c[2] & c[1] & ~c[0];
-	assign m15 = c[3] & c[2] & c[1] & c[0];
-
-	assign display[0] = ~(m0 | m2 | m3 | m5 | m6 | m7 | m8 | m9 | m10 | m12 | m14 | m15); //sum of product of minterms
-	assign display[1] = ~(m0 | m1 | m2 | m3 | m4 | m7 | m8 | m9 | m10 | m13);
-	assign display[2] = ~(m0 | m1 | m3 | m4 | m5 | m6 | m7 | m8 | m9 | m10 | m11 | m13);
-	assign display[3] = ~(m0 | m2 | m3 | m5 | m6 | m8 | m9 | m11 | m12 | m13 | m14);
-	assign display[4] = ~(m0 | m2 | m6 | m8 | m10 | m11 | m12 | m13 | m14 | m15);
-	assign display[5] = ~(m0 | m4 | m5 | m6 | m8 | m9 | m10 | m11 | m12 | m14 | m15);
-	assign display[6] = ~(m2 | m3 | m4 | m5 | m6 | m8 | m9 | m10 | m11 | m14 | m15);
-
-
-endmodule
-
-
-
-module RateDivider
-#(parameter CLOCK_FREQUENCY = 250) (
-input ClockIn,
-input Reset,
-output reg Enable
-);
-
-    reg [26:0] down_count;
- //   reg temp;
-
-//    always @ (*)
-//    begin
-//
-//	temp = CLOCK_FREQUENCY;
-//
-//    end
-
-    always @ (posedge ClockIn)
-    begin
-        if (!Reset)
-            begin
-                Enable = 1'b0; //start with enable = 1 or 0??
-                down_count <= 2500000;
-            end
-		  else if (down_count == 27'd0)
-				begin
-					Enable = 1'b1;
-					down_count <= 2500000;
-				end
-        else
-            begin
-                Enable = 1'b0;
-                down_count <= down_count - 1;
-            end
-    end
-    // assign Enable = (down_count == 27'd0)? 1'b1:1'b0;
 endmodule
